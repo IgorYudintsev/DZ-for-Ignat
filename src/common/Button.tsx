5a53b5158344b86ae6c02a2a16dc7255bf3eda62
mobile08092018 <kakvospitat@gmail.com>
2020-08-09 15:28:55 +0300

    09.09.2020-menu-monday

--- src/common/Button.tsx
+++ src/common/Button.tsx
@@ -12,19 +12,3 @@ export const Button = (props:ButtonPropsType) => {
                 onClick={props.callback}>{props.title}</button>
     )
 }
-
-
-//=================================
-// import React from "react";
-// import './Button.styles.css'
-//
-// type ButtonPropsType = {
-//     title: string
-//     callback: () => void
-// }
-//
-// export const Button = (props:ButtonPropsType) => {
-//     return (
-//         <button className={props.title==='delete'?'orangeButton': 'universalButton'} onClick={props.callback}>{props.title}</button>
-//     )
-// }
