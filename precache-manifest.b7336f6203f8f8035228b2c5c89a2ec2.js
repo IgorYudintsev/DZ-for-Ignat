2da2239ffb22890e3663b0590cef9d4a83510ec2
mobile08092018 <kakvospitat@gmail.com>
2020-08-14 08:36:16 +0300

    Updates

---
+++ precache-manifest.b7336f6203f8f8035228b2c5c89a2ec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6795cbbf51266b25410d96f174114893",
+    "url": "/DZ-for-Ignat/index.html"
+  },
+  {
+    "revision": "610f1a595235990a3fc3",
+    "url": "/DZ-for-Ignat/static/css/main.2bceee87.chunk.css"
+  },
+  {
+    "revision": "0723348b486483ddb67e",
+    "url": "/DZ-for-Ignat/static/js/2.afc95897.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DZ-for-Ignat/static/js/2.afc95897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "610f1a595235990a3fc3",
+    "url": "/DZ-for-Ignat/static/js/main.ff435202.chunk.js"
+  },
+  {
+    "revision": "f74febbcad77abd2b480",
+    "url": "/DZ-for-Ignat/static/js/runtime-main.308d4e60.js"
+  }
+]);
